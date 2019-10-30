80f317872f479dc910190410f8b6757cef80cab5
Daniel Cabezas Recio <daniel.cabezas.recio@gmail.com>
2019-10-30 12:52:12 +0100

    añadida tabla pertenecen

---
+++ Tabla_Pertenecen.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Pertenecen(
+	Nombre_Equipo VARCHAR(50) NOT NULL,
+	nif VARCHAR(10) NOT NULL,
+	PRIMARY KEY (Nombre_Equipo,nif),
+	FOREGIN KEY (Nombre_Equipo) REFERENCES Equipos(Nombre_Equipos),
+	FOREGIN KEY (nif) REFERENCES Jugadores(nif));
+
+INSERT INTO Pertenecen VALUES ("Huesca","03567892");
+INSERT INTO Pertenecen VALUES ("Real Sociedad","96234578");
+INSERT INTO Pertenecen VALUES ("Real Madrid","78456312");
