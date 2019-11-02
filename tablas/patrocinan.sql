abe351dd6aeb3c690cdc920bcbc118b43ded8ea3
Julian Perez <julianperez@MacBook-Pro-de-Julian.local>
2019-11-02 12:18:15 +0100

    tabla patrocinan

---
+++ tablas/patrocinan.sql
@@ -0,0 +1,8 @@
+.open baloncesto.db
+
+CREATE TABLE patrocinan(
+	anyo INT,
+	CIF VARCHAR(50),
+	PRIMARY KEY (anyo, CIF),
+	FOREIGN KEY (anyo) REFERENCES campeonato(anyo),
+	FOREIGN KEY (CIF) REFERENCES empresas(CIF));
