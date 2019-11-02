f8eed1369736406caf5c155bdc00264ce29798ad
victorbrobles <victorblasco7@hotmail.com>
2019-11-02 12:39:44 +0100

    Añadido empresas y campeonato

---
+++ Campeonato.sql
@@ -0,0 +1,6 @@
+
+.open baloncesto.db
+
+CREATE TABLE Campeonato (anyo INT, fecha_in VARCHAR(10), fecha_fin VARCHAR(10), PRIMARY KEY (anyo));
+
+
---
+++ Empresas.sql
@@ -0,0 +1,6 @@
+
+.open baloncesto.db
+
+CREATE TABLE Empresas (cif VARCHAR(15), nombre VARCHAR(15), PRIMARY KEY (cif));
+
+
