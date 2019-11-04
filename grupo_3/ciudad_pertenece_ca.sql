d15363c3266a39036bc95d1f391b8d152ee95393
felipecordente <felipe_1998_@outlook.com>
2019-11-04 20:52:08 +0100

    Inserts en la tabla Ciudad_pertenece_ca

--- grupo_3/ciudad_pertenece_ca.sql
+++ grupo_3/ciudad_pertenece_ca.sql
@@ -1,7 +1,15 @@
 .open baloncesto.db
-CREATE TABLE Pertenece (
+
+CREATE TABLE Ciudad_pertenece_ca (
 Comunidad_Autonoma VARCHAR(30),
 nombre VARCHAR(30),
-PRIMARY KEY (nombre),
+PRIMARY KEY (nombre, Comunidad_Autonoma),
 FOREIGN KEY (nombre) REFERENCES Ciudad(nombre),
 FOREIGN KEY (Comunidad_Autonoma) REFERENCES Comunidades_Autonomas (Comunidad_Autonoma));
+
+INSERT INTO Ciudad_pertenece_ca VALUES ('Madrid', 'Comunidad de Madrid');
+INSERT INTO Ciudad_pertenece_ca VALUES ('Valencia', 'Comunidad Valencia');
+INSERT INTO Ciudad_pertenece_ca VALUES ('Barcelona', 'Cataluña');
+INSERT INTO Ciudad_pertenece_ca VALUES ('Leon', 'Castilla y Leon');
+INSERT INTO Ciudad_pertenece_ca VALUES ('Girona', 'Cataluña');
+INSERT INTO Ciudad_pertenece_ca VALUES ('Orense', 'Galicia');
