bb9aefb29a48859a480d0db888140ba32873cf4d
Javier Bravo Donaire <47353272+JavierBravoDonaire@users.noreply.github.com>
2019-11-04 12:53:59 +0100

    Añado los inserts v0.5
    Faltan por modificar los nombres

--- grupo_5/jugador.sql
+++ grupo_5/jugador.sql
@@ -5,3 +5,24 @@ CREATE TABLE Jugador(
 	apellidos VARCHAR(100),
 	fecha_nacimiento DATE,
 	PRIMARY KEY (nif));
+	
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Santiago','Abad Viciano',DATE '1969-07-24')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Victor','Aguilar Hernández',DATE '1998-06-09')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Carlos','Alocén Arrondo',DATE '2000-12-30')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Santiago','Abad Viciano',DATE '1969-07-24')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Santiago','Abad Viciano',DATE '1969-07-24')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Santiago','Abad Viciano',DATE '1969-07-24')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Santiago','Abad Viciano',DATE '1969-07-24')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Santiago','Abad Viciano',DATE '1969-07-24')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Santiago','Abad Viciano',DATE '1969-07-24')
+INSERT INTO Jugador(nif, nombre, apellidos, fecha_nacimiento)
+	VALUES ('03948918B','Santiago','Abad Viciano',DATE '1969-07-24')
