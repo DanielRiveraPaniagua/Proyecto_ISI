311806e977d798ccbafb50fc4d9b011016469826
DanielRiveraPaniagua <danirpclase@gmail.com>
2019-11-04 22:54:45 +0100

    Add carpetas, update participan.slq add Insertar_participaciones.sql

---
+++ grupo_5/Creacion_tablas/participan.sql
@@ -0,0 +1,10 @@
+Create table participan
+(nif Char(10) NOT NULL,
+ anyio int NOT NULL,
+ FOREIGN KEY (nif) References jugadores(nif),
+	ON DELETE CASCADE,
+ FOREIGN KEY (nif) References entrenadores(nif),
+	ON DELETE CASCADE,
+ FOREIGN KEY (anyo) References campeonato(anyio),
+	ON DELETE CASCADE,
+ PRIMARY KEY(nif, anyo));
--- grupo_5/Inserts/Insertar_participaciones.sql
+++ grupo_5/Inserts/Insertar_participaciones.sql
@@ -1,16 +1,3 @@
-.open baloncesto.db
-
-Create table participan
-(nif Char(10) NOT NULL,
- anyio int NOT NULL,
- FOREIGN KEY (nif) References jugadores(nif),
-	ON DELETE CASCADE,
- FOREIGN KEY (nif) References entrenadores(nif),
-	ON DELETE CASCADE,
- FOREIGN KEY (anyo) References campeonato(anyio),
-	ON DELETE CASCADE,
- PRIMARY KEY(nif, anyo));
- 
 INSERT INTO participan(nif,anyo)
  	Values ('03948918B',2018);
 	
