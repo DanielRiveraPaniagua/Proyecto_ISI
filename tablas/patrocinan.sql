0711c38ebcc9b0ba0b729883566b603179d92295
julianperezm <julian.perezmunoz96@gmail.com>
2019-11-04 11:29:06 +0100

    Inserts patrocinan

--- tablas/patrocinan.sql
+++ tablas/patrocinan.sql
@@ -6,3 +6,8 @@ CREATE TABLE patrocinan(
 	PRIMARY KEY (anyo, CIF),
 	FOREIGN KEY (anyo) REFERENCES campeonato(anyo),
 	FOREIGN KEY (CIF) REFERENCES empresas(CIF));
+
+INSERT INTO patrocinan(anyo, CIF) VALUES (2018,'12345');
+INSERT INTO patrocinan(anyo, CIF) VALUES (2019, '12465');
+INSERT INTO patrocinan(anyo, CIF) VALUES (2020,'12421');
+
