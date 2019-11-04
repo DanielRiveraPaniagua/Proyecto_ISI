be9f70282c6c35c1cd8958434879ce6d086d21be
Oscar Escudero Arnanz <escuderoarnanzoscar@gmail.com>
2019-11-04 15:32:33 +0100

    Insert Ciudades

--- grupo_3/tabla_ciudades.sql
+++ grupo_3/tabla_ciudades.sql
@@ -2,9 +2,9 @@
 CREATE TABLE Ciudades(
 nombre VARCHAR(30),
 PRIMARY KEY(nombre));
-insert into Ciudade values ('Madrid');
-insert into Ciudade values ('Valencia');
-insert into Ciudade values ('Barcelona');
-insert into Ciudade values ('Leon');
-insert into Ciudade values ('Girona');
-insert into Ciudade values ('Orense');
+insert into Ciudades values ('Madrid');
+insert into Ciudades values ('Valencia');
+insert into Ciudades values ('Barcelona');
+insert into Ciudades values ('Leon');
+insert into Ciudades values ('Girona');
+insert into Ciudades values ('Orense');
