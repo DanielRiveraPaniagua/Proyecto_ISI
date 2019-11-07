c375d3027b63bf17237b79028364d41e1c8833df
felipecordente <felipe_1998_@outlook.com>
2019-11-07 11:36:32 +0100

    Modificado el fichero execute_queries.sh, de forma que el formato de carpetas y archivos se queden como se dijo al comienzo del proyecto

--- execute_queries.sh
+++ execute_queries.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-Grupos=$(ls -d g*)
-for grupo in $Grupos;do
-	Tablas=$(ls $grupo/Creacion_tablas/*.sql)
-	for i in $Tablas; do
-		echo $i
-		sqlite3 baloncesto.db < $i
-	done
-done
-for grupo in $Grupos;do
-	Inserts=$(ls $grupo/Inserts/*.sql)
-	for i in $Inserts; do
-		echo $i
-		sqlite3 baloncesto.db < $i
-	done
+
+for i in grupo_*/*.sql
+do
+    echo $i
+    sqlite3 < $i
 done
