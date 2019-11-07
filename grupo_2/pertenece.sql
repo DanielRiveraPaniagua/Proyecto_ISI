dd1d67d9eaf52ef5a1a429e7e26a33d10fa38f7e
migmar97 <ma.pereyra@alumnos.urjc.es>
2019-11-07 22:27:46 +0100

    Añadido inserts en pertenece y tabla tiene can

--- grupo_2/pertenece.sql
+++ grupo_2/pertenece.sql
@@ -1,7 +1,11 @@
 .open baloncesto.db
 CREATE TABLE pertenece (
-id_cancha INTEGER NOT NULL,
-nombre_comunidad TEXT NOT NULL,
-FOREIGN KEY(nombre_comunidad) REFERENCES Comunidades_Autonomas(Comunidad_Autonoma),
-FOREIGN KEY(id_cancha) REFERENCES Direccion(id_cancha)
+	id_cancha INTEGER NOT NULL,
+	nombre_comunidad TEXT NOT NULL,
+	FOREIGN KEY(nombre_comunidad) REFERENCES Comunidades_Autonomas(Comunidad_Autonoma),
+	FOREIGN KEY(id_cancha) REFERENCES Direccion(id_cancha)
 );
+
+INSERT INTO pertenece VALUES(1,'Comunidad de Madrid');
+INSERT INTO pertenece VALUES(2,'Comunidad de Madrid');
+INSERT INTO pertenece VALUES(3,'Cataluña');
--- grupo_2/tabla_tiene_dir_can.sql
+++ grupo_2/tabla_tiene_dir_can.sql
@@ -5,3 +5,7 @@ CREATE TABLE tiene_dir_can (
 	FOREIGN KEY(id_cancha) REFERENCES Direccion(id_cancha),
 	FOREIGN KEY(nombre) REFERENCES Canchas(nombre)
 );
+
+INSERT INTO tiene_dir_can VALUES(1,'Pabellón Fernando Martín');
+INSERT INTO tiene_dir_can VALUES(2,'Wizink Center');
+INSERT INTO tiene_dir_can VALUES(3,'Palau Blaugrana');
