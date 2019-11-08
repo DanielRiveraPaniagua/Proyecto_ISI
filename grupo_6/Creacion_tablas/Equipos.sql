c587f4b8212d74c6c660eb250839c0a5921fe7dd
ISMAEL <isma98.igm@gmail.com>
2019-11-08 07:44:00 +0100

    Eliminacion de las carpetas y la fusion de los ficheros de inserts y tablas

--- grupo_6/Creacion_tablas/Equipos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Equipos (
-	ID INT NOT NULL,
-	Nombre_Equipo VARCHAR(50) NOT NULL,
-	PRIMARY KEY(Nombre_Equipo)
-);
-
-
--- grupo_6/Tabla_Tienen.sql
+++ grupo_6/Tabla_Tienen.sql
@@ -1,3 +1,13 @@
+.open baloncesto.db
+
+CREATE TABLE Tienen(
+	Nombre_Equipo VARCHAR(50) NOT NULL,
+	Nombre_Ciudad VARCHAR(30) NOT NULL,
+	PRIMARY KEY(Nombre_Equipo, Nombre_Ciudad),
+	FOREIGN KEY(Nombre_Equipo) REFERENCES Equipos(Nombre_Equipo),
+	FOREIGN KEY(Nombre_Ciudad) REFERENCES Ciudades(nombre)
+);
+
 INSERT INTO Tienen VALUES ("Club Leon", "Leon");
 INSERT INTO Tienen VALUES ("Real Orense", "Orense");
 INSERT INTO Tienen VALUES ("Club Girona", "Girona");
