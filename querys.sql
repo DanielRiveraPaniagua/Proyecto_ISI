10eae41aa20564bd4d9a238fb4d035bd569483c7
Javier Bravo Donaire <47353272+JavierBravoDonaire@users.noreply.github.com>
2019-11-09 11:25:26 +0100

    Añado 2 querys más genéricas para jugador

--- querys.sql
+++ querys.sql
@@ -1,4 +1,15 @@
 .open baloncesto.db
+
+select 
+*
+from jugador as j
+
+select 
+j.nombre||" "||j.apellidos as "Jugador",
+j.fecha_nacimiento as "Fecha Nacimiento"
+from jugador as j
+where j.nombre like "%Gasol%"
+
 select
 j.nombre||" "||j.apellidos,a.nombre_equipo
 from jugador as j
@@ -12,4 +23,3 @@ join participan as p
 on j.nif = p.nif
 where p.anyo = 2020
 order by j.nombre,j.apellidos;
-
