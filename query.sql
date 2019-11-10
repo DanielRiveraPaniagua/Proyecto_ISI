7f92607dccbc1b1dda2b8f92991ef0d0dc8d7d14
felipecordente <felipe_1998_@outlook.com>
2019-11-10 01:26:32 +0100

    grupo 3 añade 3 queries

--- query.sql
+++
@@ -1,9 +0,0 @@
-.open baloncesto.db
-
-select
-Equipos.Nombre_Equipo, c.nombre, ca.Comunidad_Autonoma
-from Ciudades as c, Comunidades_Autonomas as ca, Equipos
-join Ciudad_pertenece_ca as cp
-on cp.nombre = c.nombre and cp.Comunidad_Autonoma = ca.Comunidad_Autonoma
-join Tienen
-on Tienen.Nombre_Equipo = Equipos.Nombre_Equipo and Tienen.Nombre_Ciudad = c.nombre;
